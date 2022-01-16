0df77baa276d7f0dd61e173795541084dd01f561
satishkumar2003 <96340698+satishkumar2003@users.noreply.github.com>
2022-01-16 13:47:03 +0530

    completely useless

---
+++ literally_useless.py
@@ -0,0 +1,11 @@
+import tk
+root = tk.Tk()
+root.geometry('1300X768')
+
+frame = tk.Frame(root)
+frame.pack()
+
+close_btn = tk.Button(frame,text="Press me!",command = root.destroy)
+close_btn.place(relx=0.5,rely=0.5,relheight=0.1,relwidth=0.4,anchor='n')
+
+root.mainloop()
